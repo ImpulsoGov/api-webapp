ac3ee7a84cf9441523bb9a76ad766a2a1ae0d601
Gabrielle Arruda <gabriellearrudac@gmail.com>
2022-11-17 20:41:27 -0300

    cria os outros endpoints de territorios

--- app/routers/territorios.py
+++ app/routers/territorios.py
@@ -1,28 +1,10 @@
 from fastapi import APIRouter, Depends,Form
 from typing import Optional,List
 from app.controllers import territorios
-from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordRequestForm
-from app.models.usuarios import Usuario
 import pandas as pd
 
 router = APIRouter()
-
-fake_items_db = [{"id": 1, "item_name": "Foo", "classe": 0},
-                {"id": 2, "item_name": "Bar", "classe": 0}, 
-                {"id": 3, "item_name": "Baz", "classe": 0}]
-
-@router.get("/territorios/teste")
-async def read_item(skip: int = 0, limit: int = 10):
-    itens = fake_items_db[skip : skip + limit]
-    return itens
-
-@router.get("/territorios/teste/{item_id}")
-async def read_item(item_id: int, skip: int = 0, limit: int = 10):
-    itens = fake_items_db[skip : skip + limit]
-    id = item_id
-    df = list(filter(lambda x: x['id'] == item_id, itens))
-    return df
 
 @router.get("/territorios/estabelecimentos")
 async def busca_estabelecimento(cnes: Optional[str] = None):
@@ -32,4 +14,19 @@ async def busca_estabelecimento(cnes: Optional[str] = None):
 @router.put("/territorios/estabelecimentos/{cnes}")
 async def atualiza_estabelecimento(cnes, lat, long):
     res = territorios.atualiza_estabelecimento(cnes, lat, long)
+    return res
+
+@router.get("/territorios/profissionais")
+async def busca_profissionais(cns: Optional[str] = None):
+    res = territorios.busca_profissionais(cns)
+    return res
+
+@router.get("/territorios/domicilios")
+async def busca_domicilios(id: Optional[str] = None):
+    res = territorios.busca_domicilios(id)
+    return res
+
+@router.get("/territorios/individuos")
+async def busca_individuos(domicilio_id: Optional[str] = None):
+    res = territorios.busca_individuos(domicilio_id)
     return res
