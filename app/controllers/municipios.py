486e036f496559571c566ec84f7a367f857d8e02
Gabrielle Arruda <gabriellearrudac@gmail.com>
2023-02-16 18:22:27 -0300

    Conserta consulta de estado

--- app/controllers/municipios.py
+++ app/controllers/municipios.py
@@ -4,31 +4,20 @@ Municipios = municipios.Municipios
 from .response_pages.municipios import html as response_municipios
 from fastapi.responses import HTMLResponse
 
-def consulta_municipio(id_sus,municipio_nome,estado_sigla,estado_nome):
+def consulta_municipio(id_sus,municipio_nome_normalizado,municipio_nome,estado_sigla,estado_nome,estado_nome_normalizado):
     if estado_sigla != None : estado_sigla=estado_sigla.upper()
     params_full = {
+        # Aqui eu estou mantendo o municipio e estado normalizado aceitando o nome para não quebrar aplicações, mas deve ser substituido
         "municipio_id_sus":id_sus,
         "municipio_nome_normalizado":municipio_nome,
+        # "municipio_nome_normalizado":municipio_nome_normalizado,
         "estado_sigla":estado_sigla,
+        # "estado_nome_normalizado":estado_nome_normalizado,
         "estado_nome_normalizado":estado_nome,
     }
     params = {coluna:parametro for coluna, parametro in params_full.items() if parametro!=None}
     if len(params)==0: 
         query = session.query(Municipios)
-        res = query.all()
-        return res
-    elif params_full["estado_sigla"] != None or params_full["estado_nome_normalizado"] != None:
-        query = session.query(Municipios).with_entities(
-                    Municipios.id,
-                    Municipios.estado_sigla,
-                    Municipios.estado_nome,
-                    Municipios.estado_id_ibge,
-                    Municipios.municipio_eh_capital,
-                    Municipios.municipio_id_sus,
-                    Municipios.municipio_id_ibge,
-                    Municipios.municipio_nome,
-                    Municipios.municipio_nome_normalizado
-        ).filter_by(**params)
         res = query.all()
         return res
     else:
