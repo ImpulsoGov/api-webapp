3dc7c224e2989fff52280264fe1003eb88939955
Tainá Santana <tainnaps@gmail.com>
2023-03-07 19:27:59 -0300

    adiciona timestamps no model cargo

--- app/models/cargos.py
+++ app/models/cargos.py
@@ -10,3 +10,5 @@ class Cargo(Base):
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4,comment='Chave de identificação única')
     nome = Column(String,nullable=False,comment='Nome do cargo')
     __table_args__ = {'schema': 'suporte'}
+    criacao_data = Column(TIMESTAMP,nullable=False,comment='Data de criação')
+    atualizacao_data = Column(TIMESTAMP,nullable=False,comment='Data de atualização')
