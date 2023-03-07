f428bfc5636ea2494fd9d5e05cb2e30597af3ecc
Tainá Santana <tainnaps@gmail.com>
2023-03-07 01:50:16 -0300

    atualiza coluna cargo_id e comentários de usuários sm

--- app/models/usuarios_sm.py
+++ app/models/usuarios_sm.py
@@ -7,13 +7,13 @@ from sqlalchemy import Column,String,Boolean,TIMESTAMP,ForeignKey
 
 class UsuarioSM(Base):
     __tablename__ = 'usuarios_sm'
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4,comment='Chave de Identificação Unica')   
-    municipio_id_ibge = Column(String,nullable=False,comment='Municipio de atuação do usuario')
-    cargo = Column(String,nullable=False,comment='Cargo atuação do usuario')
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4,comment='Chave de identificação única')   
+    municipio_id_ibge = Column(String,nullable=False,comment='Municipio de atuação do usuário')
+    cargo_id = Column(UUID(as_uuid=True),ForeignKey("suporte.cargos.id"),nullable=False, default=uuid.uuid4,comment='Id do cargo do usuário')
     telefone = Column(String,nullable=False,comment='Telefone de contato')
-    whatsapp = Column(Boolean,nullable=False, comment='Numero informado possui whatsapp')
-    id_usuario = Column(UUID(as_uuid=True),ForeignKey("suporte.usuarios.id"),nullable=False, default=uuid.uuid4,comment='Perfil de Acesso ID')
-    unidade_saude = Column(String,nullable=False,comment='unidade de atuação do usuario')
-    criacao_data = Column(TIMESTAMP,nullable=False,comment='Data de Criação')
-    atualizacao_data = Column(TIMESTAMP,nullable=False,comment='Data de Atualização')
+    whatsapp = Column(Boolean,nullable=False, comment='Telefone informado possui whatsapp?')
+    id_usuario = Column(UUID(as_uuid=True),ForeignKey("suporte.usuarios.id"),nullable=False, default=uuid.uuid4,comment='Id do usuário')
+    unidade_saude = Column(String,nullable=False,comment='Unidade de atuação do usuário')
+    criacao_data = Column(TIMESTAMP,nullable=False,comment='Data de criação')
+    atualizacao_data = Column(TIMESTAMP,nullable=False,comment='Data de atualização')
     __table_args__ = {'schema': 'suporte'}
