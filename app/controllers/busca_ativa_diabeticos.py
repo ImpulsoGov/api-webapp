703556f1410c79d0d8be7d211b2401241330b136
dlopes14 <donylopes@gmail.com>
2023-03-17 16:51:32 -0300

    altera retorno endpoints cronicos

--- app/controllers/busca_ativa_diabeticos.py
+++ app/controllers/busca_ativa_diabeticos.py
@@ -13,14 +13,7 @@ def diabeticos_equipe(municipio_uf,equipe):
         return erro
     '''
     try:
-        res_tupla = session.execute("SELECT  municipio_uf,consulta_e_afericao_em_dia,  cidadao_nome,  equipe_ine_atendimento,  possui_diabetes_diagnosticada,  atualizacao_data,  quadrimestre_atual,  status_em_dia,  cidadao_nome_social,  equipe_ine_cadastro,  apenas_autorreferida,  realizou_solicitacao_hemoglobina_ultimos_6_meses,  status_usuario,  cidadao_sexo,  equipe_nome_atendimento,  diagnostico_clinico,  dt_solicitacao_hemoglobina_glicada_mais_recente,  identificacao_condicao_diabetes,  dt_nascimento,  equipe_nome_cadastro,  data_ultimo_cadastro,  realizou_consulta_ultimos_6_meses,  cidadao_cpf,  estabelecimento_cnes_atendimento,  acs_nome_cadastro,  dt_ultima_consulta,  dt_consulta_mais_recente,  cidadao_cpf_dt_nascimento,  estabelecimento_cnes_cadastro,  acs_nome_visita,  se_faleceu,  municipio_id_sus,  prazo_proxima_solicitacao_hemoglobina,  cidadao_cns,  estabelecimento_nome_atendimento,  possui_diabetes_autorreferida,  se_mudou,  prazo_proxima_consulta,  estabelecimento_nome_cadastro,  criacao_data FROM impulso_previne_dados_nominais.painel_enfermeiras_lista_nominal_diabeticos where municipio_uf='"+municipio_uf+"'"+" and equipe_ine_cadastro ='"+equipe+"';").fetchall()
-        res = []
-        for item in res_tupla:
-            res.append(dict(item))
-        for item in res:
-            for key, value in item.items(): 
-                if type(value) == bool: item[key]=int(value)
-        return res
+        return session.execute("SELECT  municipio_uf,consulta_e_afericao_em_dia,  cidadao_nome,  equipe_ine_atendimento,  possui_diabetes_diagnosticada,  atualizacao_data,  quadrimestre_atual,  status_em_dia,  cidadao_nome_social,  equipe_ine_cadastro,  apenas_autorreferida,  realizou_solicitacao_hemoglobina_ultimos_6_meses,  status_usuario,  cidadao_sexo,  equipe_nome_atendimento,  diagnostico_clinico,  dt_solicitacao_hemoglobina_glicada_mais_recente,  identificacao_condicao_diabetes,  dt_nascimento,  equipe_nome_cadastro,  data_ultimo_cadastro,  realizou_consulta_ultimos_6_meses,  cidadao_cpf,  estabelecimento_cnes_atendimento,  acs_nome_cadastro,  dt_ultima_consulta,  dt_consulta_mais_recente,  cidadao_cpf_dt_nascimento,  estabelecimento_cnes_cadastro,  acs_nome_visita,  se_faleceu,  municipio_id_sus,  prazo_proxima_solicitacao_hemoglobina,  cidadao_cns,  estabelecimento_nome_atendimento,  possui_diabetes_autorreferida,  se_mudou,  prazo_proxima_consulta,  estabelecimento_nome_cadastro,  criacao_data FROM impulso_previne_dados_nominais.painel_enfermeiras_lista_nominal_diabeticos where municipio_uf='"+municipio_uf+"'"+" and equipe_ine_cadastro ='"+equipe+"';").fetchall()
     except Exception as error:
         session.rollback()
         print({"erros" : [error]})
@@ -37,14 +30,7 @@ def diabeticos_coordenacao(municipio_uf):
         return error
     '''
     try:
-        res_tupla = session.execute("SELECT  municipio_uf,consulta_e_afericao_em_dia,  cidadao_nome,  equipe_ine_atendimento,  possui_diabetes_diagnosticada,  atualizacao_data,  quadrimestre_atual,  status_em_dia,  cidadao_nome_social,  equipe_ine_cadastro,  apenas_autorreferida,  realizou_solicitacao_hemoglobina_ultimos_6_meses,  status_usuario,  cidadao_sexo,  equipe_nome_atendimento,  diagnostico_clinico,  dt_solicitacao_hemoglobina_glicada_mais_recente,  identificacao_condicao_diabetes,  dt_nascimento,  equipe_nome_cadastro,  data_ultimo_cadastro,  realizou_consulta_ultimos_6_meses,  cidadao_cpf,  estabelecimento_cnes_atendimento,  acs_nome_cadastro,  dt_ultima_consulta,  dt_consulta_mais_recente,  cidadao_cpf_dt_nascimento,  estabelecimento_cnes_cadastro,  acs_nome_visita,  se_faleceu,  municipio_id_sus,  prazo_proxima_solicitacao_hemoglobina,  cidadao_cns,  estabelecimento_nome_atendimento,  possui_diabetes_autorreferida,  se_mudou,  prazo_proxima_consulta,  estabelecimento_nome_cadastro,  criacao_data FROM impulso_previne_dados_nominais.painel_enfermeiras_lista_nominal_diabeticos where municipio_uf='"+municipio_uf+"';").fetchall()
-        res = []
-        for item in res_tupla:
-            res.append(dict(item))
-        for item in res:
-            for key, value in item.items(): 
-                if type(value) == bool: item[key]=int(value)
-        return res
+        return session.execute("SELECT  municipio_uf,consulta_e_afericao_em_dia,  cidadao_nome,  equipe_ine_atendimento,  possui_diabetes_diagnosticada,  atualizacao_data,  quadrimestre_atual,  status_em_dia,  cidadao_nome_social,  equipe_ine_cadastro,  apenas_autorreferida,  realizou_solicitacao_hemoglobina_ultimos_6_meses,  status_usuario,  cidadao_sexo,  equipe_nome_atendimento,  diagnostico_clinico,  dt_solicitacao_hemoglobina_glicada_mais_recente,  identificacao_condicao_diabetes,  dt_nascimento,  equipe_nome_cadastro,  data_ultimo_cadastro,  realizou_consulta_ultimos_6_meses,  cidadao_cpf,  estabelecimento_cnes_atendimento,  acs_nome_cadastro,  dt_ultima_consulta,  dt_consulta_mais_recente,  cidadao_cpf_dt_nascimento,  estabelecimento_cnes_cadastro,  acs_nome_visita,  se_faleceu,  municipio_id_sus,  prazo_proxima_solicitacao_hemoglobina,  cidadao_cns,  estabelecimento_nome_atendimento,  possui_diabetes_autorreferida,  se_mudou,  prazo_proxima_consulta,  estabelecimento_nome_cadastro,  criacao_data FROM impulso_previne_dados_nominais.painel_enfermeiras_lista_nominal_diabeticos where municipio_uf='"+municipio_uf+"';").fetchall()
     except Exception as error:
         session.rollback()
         print({"erros" : [error]})
--- app/controllers/busca_ativa_hipertensos.py
+++ app/controllers/busca_ativa_hipertensos.py
@@ -13,14 +13,7 @@ def hipertensos_equipe(municipio_uf,equipe):
         return error
     '''
     try:
-        res_tupla = session.execute("SELECT    dt_consulta_mais_recente,  cidadao_cpf_dt_nascimento,  estabelecimento_cnes_cadastro,  acs_nome_visita,  se_faleceu,  municipio_id_sus,  prazo_proxima_afericao_pa,  cidadao_cns,  estabelecimento_nome_atendimento,  possui_hipertensao_autorreferida,  se_mudou,  municipio_uf,  prazo_proxima_consulta,  cidadao_nome,  estabelecimento_nome_cadastro,  possui_hipertensao_diagnosticada,  criacao_data,  quadrimestre_atual,  consulta_e_afericao_em_dia,  cidadao_nome_social,  equipe_ine_atendimento,  apenas_autorreferida,  atualizacao_data,  realizou_afericao_ultimos_6_meses,  status_em_dia,  cidadao_sexo,  equipe_ine_cadastro,  diagnostico_clinico,  dt_afericao_pressao_mais_recente,  status_usuario,  dt_nascimento,  equipe_nome_atendimento,  data_ultimo_cadastro,  realizou_consulta_ultimos_6_meses,  identificacao_condicao_hipertensao,  estabelecimento_cnes_atendimento,  equipe_nome_cadastro,  dt_ultima_consulta,  cidadao_cpf,  acs_nome_cadastro FROM impulso_previne_dados_nominais.painel_enfermeiras_lista_nominal_hipertensos where municipio_uf='"+municipio_uf+"'"+" and equipe_ine_cadastro ='"+equipe+"';").fetchall()
-        res = []
-        for item in res_tupla:
-            res.append(dict(item))
-        for item in res:
-            for key, value in item.items(): 
-                if type(value) == bool: item[key]=int(value)
-        return res
+        return session.execute("SELECT    dt_consulta_mais_recente,  cidadao_cpf_dt_nascimento,  estabelecimento_cnes_cadastro,  acs_nome_visita,  se_faleceu,  municipio_id_sus,  prazo_proxima_afericao_pa,  cidadao_cns,  estabelecimento_nome_atendimento,  possui_hipertensao_autorreferida,  se_mudou,  municipio_uf,  prazo_proxima_consulta,  cidadao_nome,  estabelecimento_nome_cadastro,  possui_hipertensao_diagnosticada,  criacao_data,  quadrimestre_atual,  consulta_e_afericao_em_dia,  cidadao_nome_social,  equipe_ine_atendimento,  apenas_autorreferida,  atualizacao_data,  realizou_afericao_ultimos_6_meses,  status_em_dia,  cidadao_sexo,  equipe_ine_cadastro,  diagnostico_clinico,  dt_afericao_pressao_mais_recente,  status_usuario,  dt_nascimento,  equipe_nome_atendimento,  data_ultimo_cadastro,  realizou_consulta_ultimos_6_meses,  identificacao_condicao_hipertensao,  estabelecimento_cnes_atendimento,  equipe_nome_cadastro,  dt_ultima_consulta,  cidadao_cpf,  acs_nome_cadastro FROM impulso_previne_dados_nominais.painel_enfermeiras_lista_nominal_hipertensos where municipio_uf='"+municipio_uf+"'"+" and equipe_ine_cadastro ='"+equipe+"';").fetchall()
     except Exception as error:
         session.rollback()
         print({"erros" : [error]})
@@ -36,14 +29,7 @@ def hipertensos_coordenacao(municipio_uf):
         return error
     '''
     try:
-        res_tupla = session.execute("SELECT    dt_consulta_mais_recente,  cidadao_cpf_dt_nascimento,  estabelecimento_cnes_cadastro,  acs_nome_visita,  se_faleceu,  municipio_id_sus,  prazo_proxima_afericao_pa,  cidadao_cns,  estabelecimento_nome_atendimento,  possui_hipertensao_autorreferida,  se_mudou,  municipio_uf,  prazo_proxima_consulta,  cidadao_nome,  estabelecimento_nome_cadastro,  possui_hipertensao_diagnosticada,  criacao_data,  quadrimestre_atual,  consulta_e_afericao_em_dia,  cidadao_nome_social,  equipe_ine_atendimento,  apenas_autorreferida,  atualizacao_data,  realizou_afericao_ultimos_6_meses,  status_em_dia,  cidadao_sexo,  equipe_ine_cadastro,  diagnostico_clinico,  dt_afericao_pressao_mais_recente,  status_usuario,  dt_nascimento,  equipe_nome_atendimento,  data_ultimo_cadastro,  realizou_consulta_ultimos_6_meses,  identificacao_condicao_hipertensao,  estabelecimento_cnes_atendimento,  equipe_nome_cadastro,  dt_ultima_consulta,  cidadao_cpf,  acs_nome_cadastro FROM impulso_previne_dados_nominais.painel_enfermeiras_lista_nominal_hipertensos where municipio_uf='"+municipio_uf+"';").fetchall()
-        res = []
-        for item in res_tupla:
-            res.append(dict(item))
-        for item in res:
-            for key, value in item.items(): 
-                if type(value) == bool: item[key]=int(value)
-        return res
+        return session.execute("SELECT    dt_consulta_mais_recente,  cidadao_cpf_dt_nascimento,  estabelecimento_cnes_cadastro,  acs_nome_visita,  se_faleceu,  municipio_id_sus,  prazo_proxima_afericao_pa,  cidadao_cns,  estabelecimento_nome_atendimento,  possui_hipertensao_autorreferida,  se_mudou,  municipio_uf,  prazo_proxima_consulta,  cidadao_nome,  estabelecimento_nome_cadastro,  possui_hipertensao_diagnosticada,  criacao_data,  quadrimestre_atual,  consulta_e_afericao_em_dia,  cidadao_nome_social,  equipe_ine_atendimento,  apenas_autorreferida,  atualizacao_data,  realizou_afericao_ultimos_6_meses,  status_em_dia,  cidadao_sexo,  equipe_ine_cadastro,  diagnostico_clinico,  dt_afericao_pressao_mais_recente,  status_usuario,  dt_nascimento,  equipe_nome_atendimento,  data_ultimo_cadastro,  realizou_consulta_ultimos_6_meses,  identificacao_condicao_hipertensao,  estabelecimento_cnes_atendimento,  equipe_nome_cadastro,  dt_ultima_consulta,  cidadao_cpf,  acs_nome_cadastro FROM impulso_previne_dados_nominais.painel_enfermeiras_lista_nominal_hipertensos where municipio_uf='"+municipio_uf+"';").fetchall()
     except Exception as error:
         session.rollback()
         print({"erros" : [error]})
