25fd3141d4575ee14a2f4e2a556915fee7430c39
Taina Santana <tainnaps@gmail.com>
2023-03-31 23:50:31 -0300

    add models de atendimentos

---
+++ app/models/saude_mental/atendimentos_individuais/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa: F401
+from .caps_usuarios_perfil import PerfilUsuariosAtendimentosIndividuaisCaps
+from .caps_usuarios_perfil_resumo import (
+    ResumoPerfilUsuariosAtendimentosIndividuaisCaps,
+)
+from .por_caps import AtendimentosIndividuaisPorCaps
---
+++ app/models/saude_mental/atendimentos_individuais/caps_usuarios_perfil_resumo.py
@@ -0,0 +1,25 @@
+from sqlalchemy import Column, Date, Float, Numeric, Text
+from sqlalchemy.dialects.postgresql import CHAR, UUID
+
+from app.models import db
+
+Base = db.Base
+
+
+class ResumoPerfilUsuariosAtendimentosIndividuaisCaps(Base):
+    __tablename__ = "caps_usuarios_atendimentos_individuais_perfil_resumo"
+    id = Column(Text, primary_key=True)
+    unidade_geografica_id = Column(UUID(as_uuid=True))
+    unidade_geografica_id_sus = Column(CHAR(length=6))
+    competencia = Column(Date)
+    periodo_id = Column(UUID(as_uuid=True))
+    periodo = Column(Text)
+    nome_mes = Column(Text)
+    periodo_ordem = Column(Float)
+    sexo_predominante = Column(Text)
+    usuarios_sexo_predominante = Column(Numeric)
+    faixa_etaria_predominante = Column(Text)
+    usuarios_faixa_etaria_predominante = Column(Numeric)
+    cid_grupo_predominante = Column(Text)
+    usuarios_cid_predominante = Column(Numeric)
+    __table_args__ = {"schema": "saude_mental"}
