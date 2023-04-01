963a941b5e533b6703cb44525ae28dab7149254d
Gabrielle Arruda <gabriellearrudac@gmail.com>
2023-03-31 21:47:40 -0300

    + router reducao de danos e consultorio na rua

---
+++ app/models/saude_mental/consultorionarua/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa: F401
+from .consultorio_na_rua_atendimentos_ultimos_12meses import ConsultorionaruaAtendimentos12meses
+from .consultorio_na_rua_atendimentos import ConsultorionaruaAtendimentos
