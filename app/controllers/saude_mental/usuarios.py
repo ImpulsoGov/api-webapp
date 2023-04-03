e744337af8a4fc3b51709bc8ab020ba7f8cf5722
Gabrielle Arruda <gabriellearrudac@gmail.com>
2023-04-03 16:23:08 -0300

    adiciona controller de usuarios

---
+++ app/controllers/saude_mental/usuarios.py
@@ -0,0 +1,87 @@
+from fastapi import HTTPException
+
+from app.models import db
+from app.models.saude_mental.perfildeusuarios import ( UsuariosPerfil, UsuariosPerfilEstabelecimento)
+from app.models.saude_mental.usuariosnovos import ( UsuariosNovosPerfil, UsuariosNovosResumo)
+
+session = db.session
+
+
+def obter_usuarios_perfil(
+    municipio_id_sus: str,
+):
+    usuarios_perfil = (
+        session.query(UsuariosPerfil)
+        .filter_by(unidade_geografica_id_sus=municipio_id_sus)
+        .all()
+    )
+
+    if len(usuarios_perfil) == 0:
+        raise HTTPException(
+            status_code=404,
+            detail=(
+                "Dado perfil estabelecimento não encontrado."
+            ),
+        )
+
+    return usuarios_perfil
+
+
+def obter_usuarios_perfil_estabelecimento(
+    municipio_id_sus: str,
+):
+    obter_usuarios_perfil_estabelecimento = (
+        session.query(UsuariosPerfilEstabelecimento)
+        .filter_by(unidade_geografica_id_sus=municipio_id_sus)
+        .all()
+    )
+
+    if len(obter_usuarios_perfil_estabelecimento) == 0:
+        raise HTTPException(
+            status_code=404,
+            detail=(
+                "Dado perfil estabelecimento não encontrado."
+            ),
+        )
+
+    return obter_usuarios_perfil_estabelecimento
+
+
+def obter_usuarios_novos(
+    municipio_id_sus: str,
+):
+    usuarios_novos = (
+        session.query(UsuariosNovosPerfil)
+        .filter_by(unidade_geografica_id_sus=municipio_id_sus)
+        .all()
+    )
+
+    if len(usuarios_novos) == 0:
+        raise HTTPException(
+            status_code=404,
+            detail=(
+                "Dados usuarios novos não encontrados."
+            ),
+        )
+
+    return usuarios_novos
+
+
+def obter_usuarios_novos_resumo(
+    municipio_id_sus: str,
+):
+    usuarios_novos_resumo = (
+        session.query(UsuariosNovosResumo)
+        .filter_by(unidade_geografica_id_sus=municipio_id_sus)
+        .all()
+    )
+
+    if len(usuarios_novos_resumo) == 0:
+        raise HTTPException(
+            status_code=404,
+            detail=(
+                "Dados usuarios resumo novos não encontrados."
+            ),
+        )
+
+    return usuarios_novos_resumo
