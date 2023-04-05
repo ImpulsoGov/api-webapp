bfa3c0b74e70a5ed0ee19023d45787ee59a790cb
Taina Santana <tainnaps@gmail.com>
2023-04-05 11:21:46 -0300

    fix nomes das funções de rotas usuarios

--- app/routers/saude_mental/usuarios.py
+++ app/routers/saude_mental/usuarios.py
@@ -1,38 +1,40 @@
 from fastapi import APIRouter
 
-from app.controllers.saude_mental.usuarios import (obter_usuarios_perfil,obter_usuarios_perfil_estabelecimento,obter_usuarios_novos,obter_usuarios_novos_resumo)
+from app.controllers.saude_mental.usuarios import (
+    obter_usuarios_novos,
+    obter_usuarios_novos_resumo,
+    obter_usuarios_perfil,
+    obter_usuarios_perfil_estabelecimento,
+)
 
 router = APIRouter()
 
 
 @router.get("/saude-mental/usuarios/perfil")
-async def obter_usuarios_perfil(
+async def obter_perfil_usuarios(
     municipio_id_sus: str,
 ):
-    return obter_usuarios_perfil(
-        municipio_id_sus=municipio_id_sus
-    )
+    return obter_usuarios_perfil(municipio_id_sus=municipio_id_sus)
+
 
 @router.get("/saude-mental/usuarios/perfilestabelecimento")
-async def obter_usuarios_perfil_estabelecimento(
+async def obter_perfil_usuarios_estabelecimento(
     municipio_id_sus: str,
 ):
     return obter_usuarios_perfil_estabelecimento(
         municipio_id_sus=municipio_id_sus
     )
 
+
 @router.get("/saude-mental/usuarios/novos")
-async def obter_usuarios_novos(
+async def obter_novos_usuarios(
     municipio_id_sus: str,
 ):
-    return obter_usuarios_novos(
-        municipio_id_sus=municipio_id_sus
-    )
+    return obter_usuarios_novos(municipio_id_sus=municipio_id_sus)
+
 
 @router.get("/saude-mental/usuarios/novosresumo")
-async def obter_usuarios_novos_resumo(
+async def obter_novos_usuarios_resumo(
     municipio_id_sus: str,
 ):
-    return obter_usuarios_novos_resumo(
-        municipio_id_sus=municipio_id_sus
-    )
+    return obter_usuarios_novos_resumo(municipio_id_sus=municipio_id_sus)
