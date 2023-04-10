bee8de6e08a5bbe689617cab85913500463dc919
Gabrielle Arruda <gabriellearrudac@gmail.com>
2023-04-10 12:50:13 -0300

    Comenta internações

--- app/controllers/saude_mental/atencao_hospitalar.py
+++ app/controllers/saude_mental/atencao_hospitalar.py
@@ -3,10 +3,10 @@ from fastapi import HTTPException
 from app.models import db
 from app.models.saude_mental.atencao_hospitalar import (
     AcolhimentoNoturno,
-    InternacoesResumoAdmissoes,
-    InternacoesResumoAdmissoes12m,
-    InternacoesResumoAltas,
-    InternacoesResumoAltas12m
+    # InternacoesResumoAdmissoes,
+    # InternacoesResumoAdmissoes12m,
+    # InternacoesResumoAltas,
+    # InternacoesResumoAltas12m
 )
 
 session = db.session
@@ -31,78 +31,78 @@ def obter_acolhimento_noturno(
 
     return acolhimento_noturno
 
-def obter_internacoes_resumo_admissoes(
-    municipio_id_sus: str,
-):
-    internacoes_resumo_admissoes = (
-        session.query(InternacoesResumoAdmissoes)
-        .filter_by(unidade_geografica_id_sus=municipio_id_sus)
-        .all()
-    )
+# def obter_internacoes_resumo_admissoes(
+#     municipio_id_sus: str,
+# ):
+#     internacoes_resumo_admissoes = (
+#         session.query(InternacoesResumoAdmissoes)
+#         .filter_by(unidade_geografica_id_sus=municipio_id_sus)
+#         .all()
+#     )
 
-    if len(internacoes_resumo_admissoes) == 0:
-        raise HTTPException(
-            status_code=404,
-            detail=(
-                "Dados não encontrados",
-            ),
-        )
+#     if len(internacoes_resumo_admissoes) == 0:
+#         raise HTTPException(
+#             status_code=404,
+#             detail=(
+#                 "Dados não encontrados",
+#             ),
+#         )
 
-    return internacoes_resumo_admissoes
+#     return internacoes_resumo_admissoes
 
-def obter_internacoes_resumo_admissoes_12m(
-    municipio_id_sus: str,
-):
-    internacoes_resumo_admissoes_12m = (
-        session.query(InternacoesResumoAdmissoes12m)
-        .filter_by(unidade_geografica_id_sus=municipio_id_sus)
-        .all()
-    )
+# def obter_internacoes_resumo_admissoes_12m(
+#     municipio_id_sus: str,
+# ):
+#     internacoes_resumo_admissoes_12m = (
+#         session.query(InternacoesResumoAdmissoes12m)
+#         .filter_by(unidade_geografica_id_sus=municipio_id_sus)
+#         .all()
+#     )
 
-    if len(internacoes_resumo_admissoes_12m) == 0:
-        raise HTTPException(
-            status_code=404,
-            detail=(
-                "Dados não encontrados",
-            ),
-        )
+#     if len(internacoes_resumo_admissoes_12m) == 0:
+#         raise HTTPException(
+#             status_code=404,
+#             detail=(
+#                 "Dados não encontrados",
+#             ),
+#         )
 
-    return internacoes_resumo_admissoes_12m
+#     return internacoes_resumo_admissoes_12m
 
-def obter_internacoes_resumo_altas(
-    municipio_id_sus: str,
-):
-    internacoes_resumo_altas = (
-        session.query(InternacoesResumoAltas)
-        .filter_by(unidade_geografica_id_sus=municipio_id_sus)
-        .all()
-    )
+# def obter_internacoes_resumo_altas(
+#     municipio_id_sus: str,
+# ):
+#     internacoes_resumo_altas = (
+#         session.query(InternacoesResumoAltas)
+#         .filter_by(unidade_geografica_id_sus=municipio_id_sus)
+#         .all()
+#     )
 
-    if len(internacoes_resumo_altas) == 0:
-        raise HTTPException(
-            status_code=404,
-            detail=(
-                "Dados não encontrados",
-            ),
-        )
+#     if len(internacoes_resumo_altas) == 0:
+#         raise HTTPException(
+#             status_code=404,
+#             detail=(
+#                 "Dados não encontrados",
+#             ),
+#         )
 
-    return internacoes_resumo_altas
+#     return internacoes_resumo_altas
 
-def obter_internacoes_resumo_altas_12m(
-    municipio_id_sus: str,
-):
-    internacoes_resumo_altas_12m = (
-        session.query(InternacoesResumoAltas12m)
-        .filter_by(unidade_geografica_id_sus=municipio_id_sus)
-        .all()
-    )
+# def obter_internacoes_resumo_altas_12m(
+#     municipio_id_sus: str,
+# ):
+#     internacoes_resumo_altas_12m = (
+#         session.query(InternacoesResumoAltas12m)
+#         .filter_by(unidade_geografica_id_sus=municipio_id_sus)
+#         .all()
+#     )
 
-    if len(internacoes_resumo_altas_12m) == 0:
-        raise HTTPException(
-            status_code=404,
-            detail=(
-                "Dados não encontrados",
-            ),
-        )
+#     if len(internacoes_resumo_altas_12m) == 0:
+#         raise HTTPException(
+#             status_code=404,
+#             detail=(
+#                 "Dados não encontrados",
+#             ),
+#         )
 
-    return internacoes_resumo_altas_12m
+#     return internacoes_resumo_altas_12m
--- app/routers/saude_mental/atencao_hospitalar.py
+++ app/routers/saude_mental/atencao_hospitalar.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter
 
 from app.controllers.saude_mental.atencao_hospitalar import (
     obter_acolhimento_noturno,
-    obter_internacoes_resumo_admissoes,
-    obter_internacoes_resumo_admissoes_12m,
-    obter_internacoes_resumo_altas,
-    obter_internacoes_resumo_altas_12m
+    # obter_internacoes_resumo_admissoes,
+    # obter_internacoes_resumo_admissoes_12m,
+    # obter_internacoes_resumo_altas,
+    # obter_internacoes_resumo_altas_12m
 )
 
 router = APIRouter()
@@ -20,35 +20,35 @@ async def obter_dados_acolhimento_noturno(
     )
 
 
-@router.get("/saude-mental/atencao_hospitalar/admissoes")
-async def obter_internacoes_resumo_admissoes(
-    municipio_id_sus: str,
-):
-    return obter_internacoes_resumo_admissoes(
-        municipio_id_sus=municipio_id_sus
-    )
+# @router.get("/saude-mental/atencao_hospitalar/admissoes")
+# async def obter_internacoes_resumo_admissoes(
+#     municipio_id_sus: str,
+# ):
+#     return obter_internacoes_resumo_admissoes(
+#         municipio_id_sus=municipio_id_sus
+#     )
 
 
-@router.get("/saude-mental/atencao_hospitalar/admissoes12m")
-async def obter_dados_internacoes_resumo_admissoes_12m(
-    municipio_id_sus: str,
-):
-    return obter_internacoes_resumo_admissoes_12m(
-        municipio_id_sus=municipio_id_sus
-    )
+# @router.get("/saude-mental/atencao_hospitalar/admissoes12m")
+# async def obter_dados_internacoes_resumo_admissoes_12m(
+#     municipio_id_sus: str,
+# ):
+#     return obter_internacoes_resumo_admissoes_12m(
+#         municipio_id_sus=municipio_id_sus
+#     )
 
-@router.get("/saude-mental/atencao_hospitalar/altas")
-async def obter_dados_internacoes_resumo_altas(
-    municipio_id_sus: str,
-):
-    return obter_internacoes_resumo_altas(
-        municipio_id_sus=municipio_id_sus
-    )
+# @router.get("/saude-mental/atencao_hospitalar/altas")
+# async def obter_dados_internacoes_resumo_altas(
+#     municipio_id_sus: str,
+# ):
+#     return obter_internacoes_resumo_altas(
+#         municipio_id_sus=municipio_id_sus
+#     )
 
-@router.get("/saude-mental/atencao_hospitalar/altas12m")
-async def obter_dados_internacoes_resumo_altas_12m(
-    municipio_id_sus: str,
-):
-    return obter_internacoes_resumo_altas_12m(
-        municipio_id_sus=municipio_id_sus
-    )
+# @router.get("/saude-mental/atencao_hospitalar/altas12m")
+# async def obter_dados_internacoes_resumo_altas_12m(
+#     municipio_id_sus: str,
+# ):
+#     return obter_internacoes_resumo_altas_12m(
+#         municipio_id_sus=municipio_id_sus
+#     )
