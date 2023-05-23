d5a64c009883c90a3d6a3e43761c4934d1f7adc9
Taina Santana <tainnaps@gmail.com>
2023-05-23 11:02:04 -0300

    add model usuário ativo gênero e idade

---
+++ app/models/saude_mental/perfildeusuarios/caps_usuarios_ativos_perfil_genero_idade.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Date, Float, Text, Numeric
+from sqlalchemy.dialects.postgresql import TIMESTAMP, UUID, VARCHAR
+
+from app.models import db
+
+Base = db.Base
+
+
+class UsuarioAtivoGeneroIdade(Base):
+    __tablename__ = "caps_usuarios_ativos_perfil_genero_idade"
+    id = Column(Text, primary_key=True)
+    unidade_geografica_id = Column(UUID(as_uuid=True))
+    unidade_geografica_id_sus = Column(VARCHAR)
+    periodo_id = Column(UUID(as_uuid=True))
+    competencia = Column(Date)
+    estabelecimento_linha_perfil = Column(Text)
+    estabelecimento_linha_idade = Column(Text)
+    usuario_faixa_etaria_descricao = Column(Text)
+    ativos_mes = Column(Numeric)
+    ativo_3meses = Column(Numeric)
+    tornandose_inativos = Column(Numeric)
+    atualizacao_data = Column(TIMESTAMP(timezone=True))
+    usuario_sexo = Column(Text)
+    estabelecimento = Column(VARCHAR)
+    periodo = Column(Text)
+    nome_mes = Column(Text)
+    periodo_ordem = Column(Float)
