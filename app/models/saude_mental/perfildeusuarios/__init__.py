de5a6c314fb22e43c810025959dc64be83a1dbcc
Taina Santana <tainnaps@gmail.com>
2023-05-23 15:45:20 -0300

    atualiza model ativos por condição

--- app/models/saude_mental/perfildeusuarios/__init__.py
+++ app/models/saude_mental/perfildeusuarios/__init__.py
@@ -3,9 +3,11 @@ from .caps_usuarios_ativos_perfil import UsuariosPerfil
 from .caps_usuarios_ativos_por_estabelecimento_resumo import (
     UsuariosPerfilEstabelecimento,
 )
-from .caps_usuarios_ativos_perfil_condicao import UsuariosPerfilCondicao
+from .caps_usuarios_ativos_perfil_condicao_teste import UsuariosPerfilCondicao
 from .caps_usuarios_ativos_perfil_genero_idade_raca import (
     UsuariosPerfilIdadeRaca,
 )
-from .caps_usuarios_ativos_perfil_cid import UsuarioAtivoCID
-from .caps_usuarios_ativos_perfil_genero_idade import UsuarioAtivoGeneroIdade
+
+# from .caps_usuarios_ativos_perfil_cid import UsuarioAtivoCID
+# from .caps_usuarios_ativos_perfil_genero_idade import UsuarioAtivoGeneroIdade
+from .caps_usuarios_ativos_perfil_condicao import UsuarioAtivoCondicao
--- app/models/saude_mental/perfildeusuarios/caps_usuarios_ativos_perfil_condicao.py
+++ app/models/saude_mental/perfildeusuarios/caps_usuarios_ativos_perfil_condicao.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Date, Float, Integer, Text
+from sqlalchemy import Column, Date, Float, Text, Numeric
 from sqlalchemy.dialects.postgresql import TIMESTAMP, UUID, VARCHAR
 
 from app.models import db
@@ -6,17 +6,23 @@ from app.models import db
 Base = db.Base
 
 
-class UsuariosPerfilCondicao(Base):
-    __tablename__ = "caps_usuarios_ativos_perfil"
+class UsuarioAtivoCondicao(Base):
+    __tablename__ = "caps_usuarios_ativos_perfil_condicao"
     id = Column(Text, primary_key=True)
-    unidade_geografica_id_sus = Column(VARCHAR(length=15))
+    unidade_geografica_id = Column(UUID(as_uuid=True))
+    unidade_geografica_id_sus = Column(VARCHAR)
+    periodo_id = Column(UUID(as_uuid=True))
     competencia = Column(Date)
-    periodo = Column(Text)
-    nome_mes = Column(Text)
-    estabelecimento = Column(Text)
     estabelecimento_linha_perfil = Column(Text)
     estabelecimento_linha_idade = Column(Text)
-    usuario_abuso_substancias = Column(Text)
     usuario_situacao_rua = Column(Text)
-    __table_args__ = {"schema": "saude_mental", 'extend_existing': True}
- 
+    usuario_abuso_substancias = Column(Text)
+    ativos_mes = Column(Numeric)
+    ativos_3meses = Column(Numeric)
+    tornandose_inativos = Column(Numeric)
+    atualizacao_data = Column(TIMESTAMP(timezone=True))
+    estabelecimento = Column(VARCHAR)
+    periodo = Column(Text)
+    nome_mes = Column(Text)
+    periodo_ordem = Column(Float)
+    __table_args__ = {"schema": "saude_mental"}
