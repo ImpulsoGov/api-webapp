d2ad81b3d2108cbfb7c6f1f256aa99f95042fdd0
Taina Santana <tainnaps@gmail.com>
2023-05-23 11:05:32 -0300

    exporta novos models perfil de usuário

--- app/models/saude_mental/perfildeusuarios/__init__.py
+++ app/models/saude_mental/perfildeusuarios/__init__.py
@@ -1,5 +1,11 @@
 # flake8: noqa: F401
 from .caps_usuarios_ativos_perfil import UsuariosPerfil
-from .caps_usuarios_ativos_por_estabelecimento_resumo import UsuariosPerfilEstabelecimento
+from .caps_usuarios_ativos_por_estabelecimento_resumo import (
+    UsuariosPerfilEstabelecimento,
+)
 from .caps_usuarios_ativos_perfil_condicao import UsuariosPerfilCondicao
-from .caps_usuarios_ativos_perfil_genero_idade_raca import UsuariosPerfilIdadeRaca
+from .caps_usuarios_ativos_perfil_genero_idade_raca import (
+    UsuariosPerfilIdadeRaca,
+)
+from .caps_usuarios_ativos_perfil_cid import UsuarioAtivoCID
+from .caps_usuarios_ativos_perfil_genero_idade import UsuarioAtivoGeneroIdade
