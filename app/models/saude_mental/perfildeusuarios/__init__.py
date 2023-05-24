7cb562f336f18a6fac7942a0e335796fe2ee501c
Taina Santana <tainnaps@gmail.com>
2023-05-24 09:40:29 -0300

    add model usuário ativo por raça

--- app/models/saude_mental/perfildeusuarios/__init__.py
+++ app/models/saude_mental/perfildeusuarios/__init__.py
@@ -13,3 +13,4 @@ from .caps_usuarios_ativos_perfil_genero_idade import (
     UsuarioAtivoPorGeneroEIdade,
 )
 from .caps_usuarios_ativos_perfil_condicao import UsuarioAtivoPorCondicao
+from .caps_usuarios_ativos_perfil_raca import UsuarioAtivoPorRaca
---
+++ app/models/saude_mental/perfildeusuarios/caps_usuarios_ativos_perfil_raca.py
@@ -0,0 +1,27 @@
+from sqlalchemy import Column, Date, Float, Text, Numeric
+from sqlalchemy.dialects.postgresql import TIMESTAMP, UUID, VARCHAR
+
+from app.models import db
+
+Base = db.Base
+
+
+class UsuarioAtivoPorRaca(Base):
+    __tablename__ = "caps_usuarios_ativos_perfil_raca"
+    id = Column(Text, primary_key=True)
+    unidade_geografica_id = Column(UUID(as_uuid=True))
+    unidade_geografica_id_sus = Column(VARCHAR)
+    periodo_id = Column(UUID(as_uuid=True))
+    competencia = Column(Date)
+    estabelecimento_linha_perfil = Column(Text)
+    estabelecimento_linha_idade = Column(Text)
+    usuario_raca_cor = Column(Text)
+    ativos_mes = Column(Numeric)
+    ativos_3meses = Column(Numeric)
+    tornandose_inativos = Column(Numeric)
+    atualizacao_data = Column(TIMESTAMP(timezone=True))
+    estabelecimento = Column(VARCHAR)
+    periodo = Column(Text)
+    nome_mes = Column(Text)
+    periodo_ordem = Column(Float)
+    __table_args__ = {"schema": "saude_mental"}
