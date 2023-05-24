446f4d638c08417929c38489fe96cf9332f4f959
Taina Santana <tainnaps@gmail.com>
2023-05-24 13:36:48 -0300

    corrige nome da tabela do model abandono coortes

--- app/models/saude_mental/abandono/caps_adesao_evasao_coortes_resumo.py
+++ app/models/saude_mental/abandono/caps_adesao_evasao_coortes_resumo.py
@@ -7,7 +7,7 @@ Base = db.Base
 
 
 class AbandonoCoortes(Base):
-    __tablename__ = "_caps_adesao_evasao_coortes_resumo_1"
+    __tablename__ = "caps_adesao_evasao_coortes_resumo"
     id = Column(Text, primary_key=True)
     unidade_geografica_id = Column(UUID(as_uuid=True))
     unidade_geografica_id_sus = Column(VARCHAR(length=15))
