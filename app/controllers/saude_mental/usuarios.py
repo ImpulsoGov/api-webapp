8486a08a842e7d82a77e59685708498eee831f94
Taina Santana <tainnaps@gmail.com>
2023-05-24 09:40:47 -0300

    add controller usuários ativos por raça

--- app/controllers/saude_mental/usuarios.py
+++ app/controllers/saude_mental/usuarios.py
@@ -11,6 +11,7 @@ from app.models.saude_mental.perfildeusuarios import (
     UsuarioAtivoPorGeneroEIdade,
     # UsuarioAtivoCID,
     UsuarioAtivoPorCondicao,
+    UsuarioAtivoPorRaca,
 )
 from app.models.saude_mental.usuariosnovos import (
     UsuariosNovosPerfil,
@@ -218,6 +219,44 @@ def obter_perfil_usuarios_ativos_por_genero_e_idade(
             )
 
         return usuarios_ativos_por_genero_e_idade
+    except HTTPException as error:
+        session.rollback()
+
+        raise error
+    except (exc.SQLAlchemyError, Exception) as error:
+        session.rollback()
+
+        print({"error": str(error)})
+
+        raise HTTPException(
+            status_code=500,
+            detail=("Internal Server Error"),
+        )
+
+
+def obter_perfil_usuarios_ativos_por_raca(
+    municipio_id_sus: str,
+    linha_perfil: str,
+    linha_idade: str,
+):
+    try:
+        usuarios_ativos_por_raca = (
+            session.query(UsuarioAtivoPorRaca)
+            .filter_by(unidade_geografica_id_sus=municipio_id_sus)
+            .filter_by(estabelecimento_linha_perfil=linha_perfil)
+            .filter_by(estabelecimento_linha_idade=linha_idade)
+            .all()
+        )
+
+        if len(usuarios_ativos_por_raca) == 0:
+            raise HTTPException(
+                status_code=404,
+                detail=(
+                    "Dados de raça/cor de usuários ativos não encontrados."
+                ),
+            )
+
+        return usuarios_ativos_por_raca
     except HTTPException as error:
         session.rollback()
 
