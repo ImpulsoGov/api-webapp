513ce791346082e2588719c72cbdbcd79f8d466c
Taina Santana <tainnaps@gmail.com>
2023-05-24 21:36:36 -0300

    add rota usuários ativos por cid

--- app/routers/saude_mental/usuarios.py
+++ app/routers/saude_mental/usuarios.py
@@ -12,6 +12,7 @@ from app.controllers.saude_mental.usuarios import (
     obter_perfil_usuarios_ativos_por_condicao,
     obter_perfil_usuarios_ativos_por_genero_e_idade,
     obter_perfil_usuarios_ativos_por_raca,
+    obter_perfil_usuarios_ativos_por_cid,
 )
 from app.models.db import session
 
@@ -81,6 +82,19 @@ async def obter_raca_usuarios_ativos(
     linha_idade: str,
 ):
     return obter_perfil_usuarios_ativos_por_raca(
+        municipio_id_sus,
+        linha_perfil,
+        linha_idade,
+    )
+
+
+@router.get("/saude-mental/usuarios/perfil/cid")
+async def obter_cid_usuarios_ativos(
+    municipio_id_sus: str,
+    linha_perfil: str,
+    linha_idade: str,
+):
+    return obter_perfil_usuarios_ativos_por_cid(
         municipio_id_sus,
         linha_perfil,
         linha_idade,
