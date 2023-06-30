a07d333b79e3299d3268ddf4f26b7ac70b6a8d7f
Taina Santana <tainnaps@gmail.com>
2023-06-30 20:09:21 -0300

    feat: model usuários novos por condição

--- app/models/saude_mental/usuariosnovos/__init__.py
+++ app/models/saude_mental/usuariosnovos/__init__.py
@@ -1,5 +1,6 @@
 # flake8: noqa: F401
 from .caps_usuarios_novos_perfil import UsuariosNovosPerfil
 from .caps_usuarios_novos_perfil_cid import UsuarioNovoPorCID
+from .caps_usuarios_novos_perfil_condicao import UsuarioNovoPorCondicao
 from .caps_usuarios_novos_perfil_genero_idade import UsuarioNovoPorGeneroEIdade
 from .caps_usuarios_novos_resumo import UsuariosNovosResumo
