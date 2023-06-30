11c92de8f5b83c413e66b6b4f61d7b0c689bc083
Taina Santana <tainnaps@gmail.com>
2023-06-30 19:35:11 -0300

    feat model usuários novos por cid

--- app/models/saude_mental/usuariosnovos/__init__.py
+++ app/models/saude_mental/usuariosnovos/__init__.py
@@ -1,3 +1,4 @@
 # flake8: noqa: F401
 from .caps_usuarios_novos_perfil import UsuariosNovosPerfil
+from .caps_usuarios_novos_perfil_cid import UsuariosNovosPorCID
 from .caps_usuarios_novos_resumo import UsuariosNovosResumo
