1b7778079e7d2fe4c936f7ede42784b0a7e271f5
Taina Santana <tainnaps@gmail.com>
2023-07-06 01:18:03 -0300

    feat: model atendimento individual por CID

--- app/models/saude_mental/atendimentos_individuais/__init__.py
+++ app/models/saude_mental/atendimentos_individuais/__init__.py
@@ -1,4 +1,7 @@
 # flake8: noqa: F401
+from .caps_usuarios_atendimentos_individuais_perfil_cid import (
+    AtendimentoIndividualPorCID,
+)
 from .caps_usuarios_perfil import PerfilUsuariosAtendimentosIndividuaisCaps
 from .caps_usuarios_perfil_resumo import ResumoPerfilUsuariosAtendimentosIndividuaisCaps
 from .por_caps import AtendimentosIndividuaisPorCaps
