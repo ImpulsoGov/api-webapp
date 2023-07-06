274f5c9af6c7e55b6b72f1eecf7987be219ac035
Taina Santana <tainnaps@gmail.com>
2023-07-06 01:45:23 -0300

    feat: model atendimento individual por raça

--- app/models/saude_mental/atendimentos_individuais/__init__.py
+++ app/models/saude_mental/atendimentos_individuais/__init__.py
@@ -5,6 +5,9 @@ from .caps_usuarios_atendimentos_individuais_perfil_cid import (
 from .caps_usuarios_atendimentos_individuais_perfil_genero_idade import (
     AtendimentoIndividualPorGeneroEIdade,
 )
+from .caps_usuarios_atendimentos_individuais_perfil_raca import (
+    AtendimentoIndividualPorRaca,
+)
 from .caps_usuarios_perfil import PerfilUsuariosAtendimentosIndividuaisCaps
 from .caps_usuarios_perfil_resumo import ResumoPerfilUsuariosAtendimentosIndividuaisCaps
 from .por_caps import AtendimentosIndividuaisPorCaps
