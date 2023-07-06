03e5174dbc88f4230919c06b4a38d430192989d7
Taina Santana <tainnaps@gmail.com>
2023-07-06 01:18:50 -0300

    feat: model atendimento individual por gênero

--- app/models/saude_mental/atendimentos_individuais/__init__.py
+++ app/models/saude_mental/atendimentos_individuais/__init__.py
@@ -2,6 +2,9 @@
 from .caps_usuarios_atendimentos_individuais_perfil_cid import (
     AtendimentoIndividualPorCID,
 )
+from .caps_usuarios_atendimentos_individuais_perfil_genero_idade import (
+    AtendimentoIndividualPorGeneroEIdade,
+)
 from .caps_usuarios_perfil import PerfilUsuariosAtendimentosIndividuaisCaps
 from .caps_usuarios_perfil_resumo import ResumoPerfilUsuariosAtendimentosIndividuaisCaps
 from .por_caps import AtendimentosIndividuaisPorCaps
