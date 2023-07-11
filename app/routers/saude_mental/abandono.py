deccc33b0d53c2de9b9497d515ea2f2044c4937b
Taina Santana <tainnaps@gmail.com>
2023-07-11 11:20:50 -0300

    feat: rotas de adesão por cid e gênero/idade

--- app/routers/saude_mental/abandono.py
+++ app/routers/saude_mental/abandono.py
@@ -4,6 +4,8 @@ from app.controllers.saude_mental.abandono import (
     dados_caps_adesao_evasao_coortes_resumo,
     dados_caps_adesao_evasao_mensal,
     dados_caps_adesao_usuarios_perfil,
+    obter_perfil_evadiram_no_mes_por_cid,
+    obter_perfil_evadiram_no_mes_por_genero_e_idade,
 )
 
 router = APIRouter()
@@ -28,3 +30,25 @@ async def obter_dados_caps_adesao_evasao_coortes_resumo(
     municipio_id_sus: str,
 ):
     return dados_caps_adesao_usuarios_perfil(municipio_id_sus=municipio_id_sus)
+
+
+@router.get("/saude-mental/abandono/evadiram-no-mes/cid")
+async def obter_cid_evadiram_no_mes(
+    municipio_id_sus: str, estabelecimento: str, periodos: str
+):
+    return obter_perfil_evadiram_no_mes_por_cid(
+        municipio_id_sus=municipio_id_sus,
+        estabelecimento=estabelecimento,
+        periodos=periodos,
+    )
+
+
+@router.get("/saude-mental/abandono/evadiram-no-mes/genero-e-idade")
+async def obter_genero_e_idade_evadiram_no_mes(
+    municipio_id_sus: str, estabelecimento: str, periodos: str
+):
+    return obter_perfil_evadiram_no_mes_por_genero_e_idade(
+        municipio_id_sus=municipio_id_sus,
+        estabelecimento=estabelecimento,
+        periodos=periodos,
+    )
