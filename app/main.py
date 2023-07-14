38d9e1473bc013643331a9df3c3da687a0cd05a8
Taina Santana <tainnaps@gmail.com>
2023-07-14 18:27:29 -0300

    Renomeia e muda importação router de saúde mental

--- app/main.py
+++ app/main.py
@@ -2,8 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
-from app.routers import impulso_previne, territorios, usuarios
-from app.routers.saude_mental import router_saude_mental
+from app.routers import impulso_previne, saude_mental, territorios, usuarios
 
 app = FastAPI()
 
@@ -26,7 +25,7 @@ app.add_middleware(
 app.include_router(usuarios.router)
 app.include_router(impulso_previne.router)
 app.include_router(territorios.router)
-app.include_router(router_saude_mental)
+app.include_router(saude_mental.router)
 
 
 class Welcome(BaseModel):
