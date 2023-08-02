6683580ff2dd6be69ec35140503277428e87d2ba
gabriellearruda <gabriellearrudac@gmail.com>
2023-08-02 17:08:30 -0300

    Troca de bd dos models pt III

--- app/models/_conexao_banco.py
+++ app/models/_conexao_banco.py
@@ -1,10 +1,10 @@
 import os
 from dotenv import load_dotenv
 
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, engine_from_config
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy import inspect
 
 env_path = os.path.dirname(os.path.realpath(__file__)) + "/.env"
 load_dotenv(dotenv_path=env_path)
@@ -47,22 +47,58 @@ credencial_ = {
     },
 }
 
-def conexao_banco(projeto):
-    credencial = credencial_[projeto]
-    engine = create_engine(
-        "postgresql://{}:{}@{}:{}/{}?".format(
-            credencial["USERNAME"].strip(),
-            credencial["PASSWORD"].strip(),
-            credencial["HOSTNAME"].strip(),
-            credencial["PORT"].strip(),
-            credencial["DATABASE"].strip(),
-        ),
+
+
+engine_1 = create_engine(
+        "postgresql://postgres:UAnnT-80Gg-3m@34.68.133.244:5432/postgres?",
         connect_args={
             "options": "-c statement_timeout=100000000",
         },
         echo=True,
     )
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    Base = declarative_base()
-    return Base
+Base_1 = declarative_base()
+# import pdb; pdb.set_trace()
+Base_1.metadata.drop_all(bind=engine_1)
+Base_1.metadata.create_all(bind=engine_1)
+Session = sessionmaker(twophase=True)
+Session.configure(binds={Base_1: engine_1})
+
+engine_2 = create_engine(
+        "postgresql://api_user:EgQVEc-6201M@34.68.68.223:5432/api?",
+        connect_args={
+            "options": "-c statement_timeout=100000000",
+        },
+        echo=True,
+    )
+Base_2 = declarative_base()
+Base_2.metadata.drop_all(bind=engine_2)
+
+def conexao_banco(projeto):
+    # credencial = credencial_[projeto]
+    # engine = create_engine(
+    #     "postgresql://{}:{}@{}:{}/{}?".format(
+    #         credencial["USERNAME"].strip(),
+    #         credencial["PASSWORD"].strip(),
+    #         credencial["HOSTNAME"].strip(),
+    #         credencial["PORT"].strip(),
+    #         credencial["DATABASE"].strip(),
+    #     ),
+    #     connect_args={
+    #         "options": "-c statement_timeout=100000000",
+    #     },
+    #     echo=True,
+    # )
+
+    # engine_1 = create_engine(
+    #     "postgresql://postgres:UAnnT-80Gg-3m@34.68.133.244:5432/postgres?",
+    #     connect_args={
+    #         "options": "-c statement_timeout=100000000",
+    #     },
+    #     echo=True,
+    # )
+    # Base_1 = declarative_base()
+    # Base_1.metadata.drop_all(bind=engine_1)
+
+    # Session = sessionmaker(bind=engine)
+    # Base = declarative_base()
+    return Base_1
