94ca9c4c4f9146a963c42c548f504685d24b0ccc
NicoliSte <nickstefan4@gmail.com>
2023-08-11 10:33:55 -0300

    correção endpoint

--- app/controllers/indicadores_desempenho_score_equipes_validas.py
+++ app/controllers/indicadores_desempenho_score_equipes_validas.py
@@ -4,7 +4,38 @@ IndicadoresDesempenho = indicadores_desempenho_score_equipes_validas.Indicadores
 
 def consultar_indicadores_desempenho(municipio_uf):
    try: 
-        return session.query(IndicadoresDesempenho).filter_by(municipio_uf=municipio_uf).all()
+        return session.query(IndicadoresDesempenho).filter_by(municipio_uf=municipio_uf).with_entities(
+               IndicadoresDesempenho.municipio_nome,
+               IndicadoresDesempenho.municipio_uf,
+               IndicadoresDesempenho.periodo_codigo,
+               IndicadoresDesempenho.periodo_data_inicio,
+               IndicadoresDesempenho.periodo_data_fim,
+               IndicadoresDesempenho.indicador_ordem,
+               IndicadoresDesempenho.indicador_prioridade,
+               IndicadoresDesempenho.indicador_nome,
+               IndicadoresDesempenho.indicador_peso,
+               IndicadoresDesempenho.indicador_validade_resultado,
+               IndicadoresDesempenho.indicador_acoes_por_usuario,
+               IndicadoresDesempenho.indicador_numerador,
+               IndicadoresDesempenho.indicador_denominador_estimado,
+               IndicadoresDesempenho.indicador_denominador_utilizado_informado,
+               IndicadoresDesempenho.indicador_denominador_utilizado,
+               IndicadoresDesempenho.indicador_denominador_utilizado_tipo,
+               IndicadoresDesempenho.indicador_denominador_informado_diferenca_utilizado,
+               IndicadoresDesempenho.indicador_denominador_informado_diferenca_utilizado_formatado,
+               IndicadoresDesempenho.indicador_nota,
+               IndicadoresDesempenho.indicador_nota_porcentagem,
+               IndicadoresDesempenho.indicador_meta,
+               IndicadoresDesempenho.indicador_diferenca_meta,
+               IndicadoresDesempenho.indicador_recomendacao,
+               IndicadoresDesempenho.delta,
+               IndicadoresDesempenho.delta_formatado,
+               IndicadoresDesempenho.indicador_usuarios_100_porcento_meta,
+               IndicadoresDesempenho.indicador_usuarios_cadastrados_sem_atendimento,
+               IndicadoresDesempenho.indicador_usuarios_cadastrar_para_meta,
+               IndicadoresDesempenho.indicador_score,
+               IndicadoresDesempenho.indicador_denominador_informado
+            ).all()
    except Exception as error:
         session.rollback()
         print({"erros" : [error]})
