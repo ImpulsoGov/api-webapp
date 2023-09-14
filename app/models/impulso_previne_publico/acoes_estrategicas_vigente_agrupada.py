6e4fb4620da4f4cc6af5217340beb39856b64a31
NicoliSte <nickstefan4@gmail.com>
2023-09-14 14:52:12 -0300

    correção endpoints

--- app/models/impulso_previne_publico/acoes_estrategicas_vigente_agrupada.py
+++ app/models/impulso_previne_publico/acoes_estrategicas_vigente_agrupada.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, PrimaryKeyConstraint
+from sqlalchemy import Column, String, PrimaryKeyConstraint,Integer
 from app.models import db
 from app.models import db
 Base = db.Base_impulso_previne_publico
@@ -9,6 +9,7 @@ class AcoesEstrategicasVigenteAgrupada(Base):
     __tablename__ = 'acoes_estrategicas_vigente_agrupada'
     municipio_uf = Column(String,primary_key=True)
     acao_nome = Column(String,primary_key=True)
+    acumulado_12meses = Column(Integer)
     nivel_repasse = Column(String)
     periodicidade = Column(String)
     ultimo_pagamento = Column(String)
