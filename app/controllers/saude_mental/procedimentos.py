e3fd0f28fce043882e47e2b03b7237e751f11655
Taina Santana <tainnaps@gmail.com>
2023-11-01 18:20:21 -0300

    Remove endpoints de procedimentos com parquet

--- app/controllers/saude_mental/procedimentos.py
+++ app/controllers/saude_mental/procedimentos.py
@@ -1,5 +1,4 @@
-import pandas as pd
-from fastapi import HTTPException, Response
+from fastapi import HTTPException
 
 from app.models import db
 from app.models.saude_mental.procedimentos import (
@@ -44,77 +43,6 @@ def dados_procedimentos_por_usuario_resumo(municipio_id_sus: str):
         )
 
     return ProcedimentoPorUsuarioResumo_dados
-
-
-def dados_procedimentos_por_usuario_tempo_servico(municipio_id_sus: str):
-    # procedimentos_por_usuario_por_tempo_servico = (
-    #     session.query(ProcedimentoPorUsuarioTempoServiço)
-    #     .filter_by(unidade_geografica_id_sus=municipio_id_sus)
-    #     .all()
-    # )
-
-    procedimentos_por_usuario_por_tempo_servico = pd.read_parquet(
-        f"data/caps_procedimentos_por_usuario_por_tempo_servico_{municipio_id_sus}.parquet",
-    ).query(
-        "competencia > @pd.Timestamp(2022, 11, 1).date()"
-    )
-
-    if len(procedimentos_por_usuario_por_tempo_servico) == 0:
-        raise HTTPException(
-            status_code=404,
-            detail="Matriciamentos no último ano do município não encontrados",
-        )
-
-    return Response(
-        procedimentos_por_usuario_por_tempo_servico.to_json(orient="records"),
-        media_type="application/json",
-    )
-
-
-def dados_procedimentos_por_hora(municipio_id_sus: str):
-    # procedimentos_por_hora = (
-    #     session.query(ProcedimentosPorHora)
-    #     .filter_by(unidade_geografica_id_sus=municipio_id_sus)
-    #     .all()
-    # )
-    procedimentos_por_hora = pd.read_parquet(
-        f"data/caps_procedimentos_por_hora_resumo_{municipio_id_sus}.parquet",
-    ).query("competencia > @pd.Timestamp(2022, 2, 1).date()")
-
-    if len(procedimentos_por_hora) == 0:
-        raise HTTPException(
-            status_code=404,
-            detail="Matriciamentos no último ano do município não encontrados",
-        )
-
-    return Response(
-        procedimentos_por_hora.to_json(orient="records"),
-        media_type="application/json",
-    )
-
-
-def dados_procedimentos_por_tipo(municipio_id_sus: str):
-    # procedimentos_por_tipo = (
-    #     session.query(ProcedimentosPorTipo)
-    #     .filter_by(unidade_geografica_id_sus=municipio_id_sus)
-    #     .all()
-    # )
-    procedimentos_por_tipo = pd.read_parquet(
-        f"data/caps_procedimentos_por_tipo_{municipio_id_sus}.parquet",
-    ).query(
-        "competencia > @pd.Timestamp(2022, 2, 1).date()"
-    )
-
-    if len(procedimentos_por_tipo) == 0:
-        raise HTTPException(
-            status_code=404,
-            detail="Matriciamentos no último ano do município não encontrados",
-        )
-
-    return Response(
-        procedimentos_por_tipo.to_json(orient="records"),
-        media_type="application/json",
-    )
 
 
 def consultar_procedimentos_por_hora(
