c0bf1aa8f8133be20dd7ef77eea90104c865e398
Joao Vittor Souza <jvns2010@gmail.com>
2023-11-22 04:31:15 -0300

    Atualiza models de endpoints mais lentos

--- app/models/saude_mental/atendimentos_individuais/por_caps.py
+++ app/models/saude_mental/atendimentos_individuais/por_caps.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Date, Float, Numeric, Text
-from sqlalchemy.dialects.postgresql import CHAR, UUID
+from sqlalchemy.dialects.postgresql import VARCHAR, UUID
 from app.models.saude_mental.schema import SCHEMA_SAUDE_MENTAL
 
 from app.models import db
@@ -11,7 +11,7 @@ class AtendimentosIndividuaisPorCaps(Base):
     __tablename__ = "caps_atendimentos_individuais_por_caps"
     id = Column(Text, primary_key=True)
     unidade_geografica_id = Column(UUID(as_uuid=True))
-    unidade_geografica_id_sus = Column(CHAR(length=6))
+    unidade_geografica_id_sus = Column(VARCHAR(length=6))
     periodo_id = Column(UUID(as_uuid=True))
     competencia = Column(Date)
     estabelecimento_linha_perfil = Column(Text)
--- app/models/saude_mental/procedimentos/caps_procedimentos_por_usuario_por_estabelecimento.py
+++ app/models/saude_mental/procedimentos/caps_procedimentos_por_usuario_por_estabelecimento.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Date, Float, Integer, Text
+from sqlalchemy import Column, Date, Float, Integer, Text, Numeric
 from sqlalchemy.dialects.postgresql import TIMESTAMP, UUID, VARCHAR
 from app.models.saude_mental.schema import SCHEMA_SAUDE_MENTAL
 
@@ -10,7 +10,7 @@ Base = db.Base_saude_mental
 class ProcedimentoPorUsuarioEstabelecimento(Base):
     __tablename__ = "caps_procedimentos_por_usuario_por_estabelecimento"
     id = Column(Text, primary_key=True)
-    unidade_geografica_id_sus = Column(VARCHAR(length=15))
+    unidade_geografica_id_sus = Column(VARCHAR(length=6))
     competencia = Column(Date)
     periodo = Column(Text)
     nome_mes = Column(Text)
@@ -21,9 +21,9 @@ class ProcedimentoPorUsuarioEstabelecimento(Base):
     # procedimentos_exceto_acolhimento_anterior = Column(Float)
     # ativos_mes = Column(Float)
     # ativos_mes_anterior = Column(Float)
-    procedimentos_por_usuario = Column(Float)
+    procedimentos_por_usuario = Column(Numeric)
     # procedimentos_por_usuario_anterior = Column(Float)
     # maior_taxa = Column(Float)
     # maior_taxa_anterior = Column(Float)
-    dif_procedimentos_por_usuario_anterior_perc = Column(Float)
+    dif_procedimentos_por_usuario_anterior_perc = Column(Numeric)
     __table_args__ = {"schema": SCHEMA_SAUDE_MENTAL}
