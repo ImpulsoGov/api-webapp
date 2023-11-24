62d9e21b67c3707d249af77f0fae17e59333d7a0
Taina Santana <tainnaps@gmail.com>
2023-11-24 10:17:07 -0300

    Add rollback controllers ambulatório

--- app/controllers/saude_mental/ambulatorio.py
+++ app/controllers/saude_mental/ambulatorio.py
@@ -10,6 +10,7 @@ from app.utils.separar_string import separar_string
 
 session = db.session
 
+
 def consultar_dados_ambulatorio_atendimento_resumo(
     municipio_id_sus: str,
     estabelecimentos: str,
@@ -44,37 +45,46 @@ def consultar_dados_ambulatorio_atendimento_resumo(
 def obter_ambulatorio_atendimento_resumo_ultimo_mes(
     municipio_id_sus: str,
 ):
-    ambulatorio_atendimento_resumo_ultimo_mes = (
-        session.query(AmbulatorioAtendimentoResumoUltimoMes)
-        .filter_by(unidade_geografica_id_sus=municipio_id_sus)
-        .all()
-    )
-
-    if len(ambulatorio_atendimento_resumo_ultimo_mes) == 0:
-        raise HTTPException(
-            status_code=404,
-            detail=("Dados não encontrados",),
+    try:
+        ambulatorio_atendimento_resumo_ultimo_mes = (
+            session.query(AmbulatorioAtendimentoResumoUltimoMes)
+            .filter_by(unidade_geografica_id_sus=municipio_id_sus)
+            .all()
         )
 
-    return ambulatorio_atendimento_resumo_ultimo_mes
+        return ambulatorio_atendimento_resumo_ultimo_mes
+    except (Exception) as error:
+        session.rollback()
+
+        print({"error": str(error)})
+
+        raise HTTPException(
+            status_code=500,
+            detail=("Internal Server Error"),
+        )
 
 
 def obter_ambulatorio_procedimento_por_profissional(
     municipio_id_sus: str,
 ):
-    ambulatorio_procedimento_por_profissional = (
-        session.query(AmbulatorioProcedimentosPorProfissional)
-        .filter_by(unidade_geografica_id_sus=municipio_id_sus)
-        .all()
-    )
-
-    if len(ambulatorio_procedimento_por_profissional) == 0:
-        raise HTTPException(
-            status_code=404,
-            detail=("Dados não encontrados",),
+    try:
+        ambulatorio_procedimento_por_profissional = (
+            session.query(AmbulatorioProcedimentosPorProfissional)
+            .filter_by(unidade_geografica_id_sus=municipio_id_sus)
+            .all()
         )
 
-    return ambulatorio_procedimento_por_profissional
+        return ambulatorio_procedimento_por_profissional
+    except (Exception) as error:
+        session.rollback()
+
+        print({"error": str(error)})
+
+        raise HTTPException(
+            status_code=500,
+            detail=("Internal Server Error"),
+        )
+
 
 def consultar_ambulatorio_usuario_perfil(
     municipio_id_sus: str,
@@ -93,7 +103,7 @@ def consultar_ambulatorio_usuario_perfil(
                 )
         if periodos is not None:
                 lista_periodos = separar_string("-", periodos)
-                query = query.filter(AmbulatorioUsuariosPerfil.periodo.in_(lista_periodos))        
+                query = query.filter(AmbulatorioUsuariosPerfil.periodo.in_(lista_periodos))
 
         ambulatorio_usuario_perfil = query.all()
         return ambulatorio_usuario_perfil
