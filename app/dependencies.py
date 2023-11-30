c1ec5c909da6735eb205cdb7ab4bf70b0c83e99a
João Vittor <joao.vittor@eng.ci.ufpb.br>
2023-11-30 14:07:12 -0300

    Remove import inutilizado

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,7 +1,10 @@
-from fastapi import Request, Response
+from fastapi import Response
 
 QUANTIDADE_SEGUNDOS_24_HORAS = 60 * 60 * 24
 
-async def adiciona_cache_header(request: Request, response: Response):
-    response.headers["Cache-Control"] = f"private, max-age={QUANTIDADE_SEGUNDOS_24_HORAS}"
+
+async def adiciona_cache_header(response: Response):
+    response.headers[
+        "Cache-Control"
+    ] = f"private, max-age={QUANTIDADE_SEGUNDOS_24_HORAS}"
     return response
