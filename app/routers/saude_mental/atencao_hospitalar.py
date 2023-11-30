cc1934a45cf699b76a46b95658eaea2945fc9fc6
João Vittor <joao.vittor@eng.ci.ufpb.br>
2023-11-30 14:13:21 -0300

    remove comentarios inutilizados

--- app/routers/saude_mental/atencao_hospitalar.py
+++ app/routers/saude_mental/atencao_hospitalar.py
@@ -23,26 +23,8 @@ async def obter_internacoes_resumo_admissoes(
     return obter_internacoes_resumo_admissoes(municipio_id_sus=municipio_id_sus)
 
 
-# @router.get("/saude-mental/atencao_hospitalar/admissoes12m")
-# async def obter_dados_internacoes_resumo_admissoes_12m(
-#     municipio_id_sus: str,
-# ):
-#     return obter_internacoes_resumo_admissoes_12m(
-#         municipio_id_sus=municipio_id_sus
-#     )
-
-
 @router.get("/saude-mental/atencao_hospitalar/altas")
 async def obter_dados_internacoes_resumo_altas(
     municipio_id_sus: str,
 ):
     return obter_internacoes_resumo_altas(municipio_id_sus=municipio_id_sus)
-
-
-# @router.get("/saude-mental/atencao_hospitalar/altas12m")
-# async def obter_dados_internacoes_resumo_altas_12m(
-#     municipio_id_sus: str,
-# ):
-#     return obter_internacoes_resumo_altas_12m(
-#         municipio_id_sus=municipio_id_sus
-#     )
