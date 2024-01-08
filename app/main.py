0befd988069b15817c87c5f5c28b1e89bd02091a
Taina Santana <tainnaps@gmail.com>
2024-01-08 16:24:15 -0300

    Remove comentários

--- app/main.py
+++ app/main.py
@@ -13,12 +13,7 @@ from app.routers import (
 
 sentry_sdk.init(
     dsn="https://2bf90390fba62a2238c1b372df068142@o4506520445517824.ingest.sentry.io/4506537208250368",
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
     traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100%
-    # of sampled transactions.
-    # We recommend adjusting this value in production.
     profiles_sample_rate=1.0,
 )
 
