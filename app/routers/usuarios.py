bf3e8485865cd13d6c55ac630d989048b05dc792
Tainá Santana <tainnaps@gmail.com>
2024-02-20 18:43:06 -0300

    [Gestão de usuários lotes] Atualiza municipio_id_sus  (#153)
    * Add exceções personalizadas
    
    * Add municipio_id_sus no cadastro em lotes sem ativação
    
    * Formata código
    
    * Remove prints de teste
    
    * Add id sus obrigatório e valida município e id sus
    
    * Trata exceções genéricas
    
    * Revert "Trata exceções genéricas"
    
    This reverts commit 01b65f5edf4e6e890e46f4b4b3978e3ba2f4fd0b.
    
    * Valida existência de município no banco
    
    * Renomeia função de rota cadastro lotes sem ativação

--- app/routers/usuarios.py
+++ app/routers/usuarios.py
@@ -143,7 +143,7 @@ async def cadastro_lotes(
 
 
 @router.post("/suporte/usuarios/cadastro-lote-sem-ativacao")
-async def cadastro_lotes(
+async def cadastro_lotes_sem_ativacao(
     nome: str = Form(...),
     mail: str = Form(...),
     cpf: str = Form(...),
@@ -155,7 +155,7 @@ async def cadastro_lotes(
     perfil: str = Form(...),
     projeto: Optional[str] = Form("IP"),
     unidade_saude: Optional[str] = Form(None),
-    municipio_id_ibge: Optional[str] = Form(None),
+    municipio_id_sus: str = Form(...),
     username: Usuario = Depends(auth.get_current_user),
 ):
     return cadastro_usuarios.cadastrar_em_lote_sem_ativacao(
@@ -172,7 +172,7 @@ async def cadastro_lotes(
         perfil=perfil,
         projeto=projeto,
         unidade_saude=unidade_saude,
-        municipio_id_ibge=municipio_id_ibge,
+        municipio_id_sus=municipio_id_sus,
     )
 
 
