d5a193fffa6214609cce5e8b668ebd3e2e77f808
Taina Santana <tainnaps@gmail.com>
2024-02-28 10:28:15 -0300

    Refatora testes de get_user

--- tests/unit/controllers/usuarios/auth/test_get_user.py
+++ tests/unit/controllers/usuarios/auth/test_get_user.py
@@ -1,13 +1,27 @@
 from unittest.mock import patch
 from datetime import datetime
-from app.controllers.usuarios.auth import get_user
+from app.controllers.usuarios import auth
+from app.models import db
 
-MOCK_USER = {
+# TODO: padronizar uso do patch
+# TODO: usar função util que encadeia métodos
+
+MOCK_USER_1 = {
     "id": "1",
-    "nome_usuario": "Usuário Teste",
+    "nome_usuario": "Usuário Teste 1",
     "hash_senha": "",
-    "mail": "teste@email.com",
+    "mail": "teste1@email.com",
     "cpf": "00000000000",
+    "perfil_ativo": True,
+    "criacao_data": datetime.now(),
+    "atualizacao_data": datetime.now(),
+}
+MOCK_USER_2 = {
+    "id": "2",
+    "nome_usuario": "Usuário Teste 2",
+    "hash_senha": "",
+    "mail": "teste2@email.com",
+    "cpf": "11111111111",
     "perfil_ativo": True,
     "criacao_data": datetime.now(),
     "atualizacao_data": datetime.now(),
@@ -15,26 +29,26 @@ MOCK_USER = {
 
 
 def test_get_user_db_returns_list_of_users():
-    with patch("app.controllers.usuarios.auth.db") as mock_db:
+    with patch.object(auth, "db") as mock_db:
         config = {
             "session.query.return_value.filter_by.return_value.all.return_value": (
-                [MOCK_USER]
+                [MOCK_USER_1, MOCK_USER_2]
             )
         }
         mock_db.configure_mock(**config)
-        result = get_user(cpf="00000000000")
-    assert result == MOCK_USER
+        result = auth.get_user(cpf="00000000000")
+    assert result == MOCK_USER_1
 
 
 def test_get_user_db_raises_exception():
-    with patch("app.controllers.usuarios.auth.db") as mock_db:
+    with patch.object(db, "session", autospec=True) as mock_session:
         config = {
-            "session.query.return_value.filter_by.return_value.all.side_effect": (
+            "query.return_value.filter_by.return_value.all.side_effect": (
                 Exception("Error")
             )
         }
-        mock_db.configure_mock(**config)
-        result = get_user(cpf="00000000000")
+        mock_session.configure_mock(**config)
+        result = auth.get_user(cpf="00000000000")
     assert result is None
 
 
@@ -44,5 +58,5 @@ def test_get_user_db_returns_empty_list():
             "session.query.return_value.filter_by.return_value.all.return_value": []
         }
         mock_db.configure_mock(**config)
-        result = get_user(cpf="00000000000")
+        result = auth.get_user(cpf="00000000000")
     assert result is None
