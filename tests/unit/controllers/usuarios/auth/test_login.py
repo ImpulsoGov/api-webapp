c24df980c1d65b5efe8dd2005ef2d5c606b1e394
Taina Santana <tainnaps@gmail.com>
2024-03-05 12:28:56 -0300

    Add testes unitários função login

---
+++ tests/unit/controllers/usuarios/auth/test_login.py
@@ -0,0 +1,70 @@
+from unittest.mock import patch, DEFAULT
+from app.controllers.usuarios import auth
+from dataclasses import dataclass
+import pytest
+import fastapi
+
+BAD_REQUEST_RESPONSE_HEADER = {"WWW-Authenticate": "Bearer"}
+
+
+@dataclass
+class LoginCredentials:
+    username: str = "12345678900"
+    password: str = "123456"
+
+
+@pytest.fixture
+def login_credentials():
+    return LoginCredentials()
+
+
+def test_login_success(login_credentials, token):
+    with patch.multiple(
+        auth,
+        autenticar=DEFAULT,
+        criar_token=DEFAULT,
+        autospec=True,
+    ) as mocks_auth:
+        mocks_auth["autenticar"].return_value = login_credentials.username
+        mocks_auth["criar_token"].return_value = token
+        result = auth.login(form_data=login_credentials)
+    assert result == {"access_token": token, "token_type": "bearer"}
+
+
+def test_incorrect_cpf(login_credentials):
+    with patch.object(
+        auth, "autenticar", autospec=True
+    ) as mock_autenticar, pytest.raises(fastapi.HTTPException) as exec_info:
+        incorrect_cpf_code = 1
+        mock_autenticar.return_value = incorrect_cpf_code
+        auth.login(form_data=login_credentials)
+    assert exec_info.type is fastapi.HTTPException
+    assert exec_info.value.status_code == fastapi.status.HTTP_400_BAD_REQUEST
+    assert exec_info.value.headers == BAD_REQUEST_RESPONSE_HEADER
+    assert exec_info.value.detail == "CPF Incorreto"
+
+
+def test_incorrect_password(login_credentials):
+    with patch.object(
+        auth, "autenticar", autospec=True
+    ) as mock_autenticar, pytest.raises(fastapi.HTTPException) as exec_info:
+        incorrect_password_code = 2
+        mock_autenticar.return_value = incorrect_password_code
+        auth.login(form_data=login_credentials)
+    assert exec_info.type is fastapi.HTTPException
+    assert exec_info.value.status_code == fastapi.status.HTTP_400_BAD_REQUEST
+    assert exec_info.value.headers == BAD_REQUEST_RESPONSE_HEADER
+    assert exec_info.value.detail == "Senha Inválida"
+
+
+def test_user_is_inactive(login_credentials):
+    with patch.object(
+        auth, "autenticar", autospec=True
+    ) as mock_autenticar, pytest.raises(fastapi.HTTPException) as exec_info:
+        inactive_user_code = 3
+        mock_autenticar.return_value = inactive_user_code
+        auth.login(form_data=login_credentials)
+    assert exec_info.type is fastapi.HTTPException
+    assert exec_info.value.status_code == fastapi.status.HTTP_400_BAD_REQUEST
+    assert exec_info.value.headers == BAD_REQUEST_RESPONSE_HEADER
+    assert exec_info.value.detail == "Usuário Inativo"
