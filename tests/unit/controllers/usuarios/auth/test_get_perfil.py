f0eacec97a7f7d9d4b425e416f863a034116b3ae
Taina Santana <tainnaps@gmail.com>
2024-03-06 17:31:54 -0300

    Formata config e testa tipo de retorno get_perfil

--- tests/unit/controllers/usuarios/auth/test_get_perfil.py
+++ tests/unit/controllers/usuarios/auth/test_get_perfil.py
@@ -2,7 +2,6 @@ from unittest.mock import patch
 from app.controllers.usuarios.auth import get_perfil
 from app.models import db
 
-# TODO: usar função util que encadeia métodos
 # TODO: adicionar validação de existência do retorno do banco com respostas descritivas
 
 
@@ -12,9 +11,14 @@ def test_db_returns_list_of_user_profile(
 ):
     with patch.object(db, "session", autospec=True) as mock_session:
         config = {
-            "query.return_value.join.return_value.join.return_value.with_entities.return_value.filter_by.return_value.all.return_value": (
-                [user_1_profile_1, user_1_profile_2]
-            )
+            (
+                "query.return_value"
+                ".join.return_value"
+                ".join.return_value"
+                ".with_entities.return_value"
+                ".filter_by.return_value"
+                ".all.return_value"
+            ): ([user_1_profile_1, user_1_profile_2])
         }
         mock_session.configure_mock(**config)
         result = get_perfil(cpf="00000000000")
@@ -28,12 +32,18 @@ def test_db_returns_list_of_user_profile(
 def test_db_returns_empty_list():
     with patch.object(db, "session", autospec=True) as mock_session:
         config = {
-            "query.return_value.join.return_value.join.return_value.with_entities.return_value.filter_by.return_value.all.return_value": (
-                []
-            )
+            (
+                "query.return_value"
+                ".join.return_value"
+                ".join.return_value"
+                ".with_entities.return_value"
+                ".filter_by.return_value"
+                ".all.return_value"
+            ): ([])
         }
         mock_session.configure_mock(**config)
         result = get_perfil(cpf="00000000000")
+    assert isinstance(result, dict)
     assert "erros" in result.keys()
 
 
@@ -41,11 +51,17 @@ def test_db_raises_exception():
     with patch.object(db, "session", autospec=True) as mock_session:
         error = Exception("An error happened")
         config = {
-            "query.return_value.join.return_value.join.return_value.with_entities.return_value.filter_by.return_value.all.side_effect": (
-                error
-            )
+            (
+                "query.return_value"
+                ".join.return_value"
+                ".join.return_value"
+                ".with_entities.return_value"
+                ".filter_by.return_value"
+                ".all.side_effect"
+            ): (error)
         }
         mock_session.configure_mock(**config)
         result = get_perfil(cpf="00000000000")
+    assert isinstance(result, dict)
     assert "erros" in result.keys()
     assert error in result["erros"]
