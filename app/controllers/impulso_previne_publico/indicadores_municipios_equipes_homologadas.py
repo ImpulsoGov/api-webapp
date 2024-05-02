8b3a94c2d2115c0326e74028dd3b591e94b6a3e1
dlopes14 <donylopes@gmail.com>
2024-05-02 17:21:11 -0300

    adiciona entidades controller indicadores de desempenho

--- app/controllers/impulso_previne_publico/indicadores_municipios_equipes_homologadas.py
+++ app/controllers/impulso_previne_publico/indicadores_municipios_equipes_homologadas.py
@@ -14,6 +14,24 @@ def consultar_indicadores_municipios_equipes_homologadas(municipio_uf=None):
         return (
             session.query(IndicadoresMunicipiosEquipesHomologadas)
             .filter_by(municipio_uf=municipio_uf)
+            .with_entities(
+                IndicadoresMunicipiosEquipesHomologadas.periodo_data_inicio,
+                IndicadoresMunicipiosEquipesHomologadas.periodo_data_fim,
+                IndicadoresMunicipiosEquipesHomologadas.periodo_codigo,
+                IndicadoresMunicipiosEquipesHomologadas.municipio_id_ibge,
+                IndicadoresMunicipiosEquipesHomologadas.municipio_id_sus,
+                IndicadoresMunicipiosEquipesHomologadas.municipio_nome,
+                IndicadoresMunicipiosEquipesHomologadas.municipio_uf,
+                IndicadoresMunicipiosEquipesHomologadas.indicador_ordem,
+                IndicadoresMunicipiosEquipesHomologadas.indicador_nome,
+                IndicadoresMunicipiosEquipesHomologadas.indicador_meta,
+                IndicadoresMunicipiosEquipesHomologadas.indicador_peso,
+                IndicadoresMunicipiosEquipesHomologadas.indicador_numerador,
+                IndicadoresMunicipiosEquipesHomologadas.indicador_denominador_informado,
+                IndicadoresMunicipiosEquipesHomologadas.indicador_denominador_estimado,
+                IndicadoresMunicipiosEquipesHomologadas.indicador_denominador_utilizado,
+                IndicadoresMunicipiosEquipesHomologadas.indicador_resultado,
+            )
             .all()
         )
     except Exception as error:
